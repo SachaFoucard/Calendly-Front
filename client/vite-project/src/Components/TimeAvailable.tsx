30bd954187741ab38be093077a3143e4803d7e59
sacha foucard <94567706+SachaFoucard@users.noreply.github.com>
2024-12-23 13:53:54 +0200

    Update TimeAvailable.tsx

--- client/vite-project/src/Components/TimeAvailable.tsx
+++ client/vite-project/src/Components/TimeAvailable.tsx
@@ -17,7 +17,7 @@ export default function TimeAvailable() {
             const cleanedDate = isoDay.split('+')[0];
             
             const response = await fetch(
-                `https://calendly-back.onrender.com/api/meetings/day?day=${cleanedDate}`
+                `https://calendy-back.vercel.app/api/meetings/day?day=${cleanedDate}`
             );
             if (!response.ok) {
                 console.error('Failed to fetch meetings:', response.statusText);
